4d88e171a8af7c9c4f0ed6a809b9d8000dce687d
michael.coote <michael.coote@poweradvocate.com>
2016-01-21 11:49:52 -0500

    wk3 edited cache and solv functions

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Caches the inverse of a matrix
+
+## creates a special matrix which sets and gets the inverse of the matrix (R solve function)
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) m <<- solve
+        getSolve <- function() m
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
+}
+
+
+## Calculates the inverse of the special matrix created above, first checking for a chached
+## soluction.
+
+cacheSolve <- function(x, ...) {
+        m <- x$getSolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setSolve(m)
+        m
+}
